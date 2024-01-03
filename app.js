6b1c47f0bc683921725275211864c6dc2155b441
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-01-02 23:50:44 -0600

    saving

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // This module enables us to use the SHA-256 hash
 import crypto from 'crypto'
+// Stands for "file system", this allows us to read/write files, including deleting them
 import fs from 'fs'
+// To access the directory of the images
+import path from 'path'
+
+// Maybe take user input for the directory
+const targetDirectory = "./Put_Images_Here"
+
+/**
+ * Funciton to calculate the hash for an image
+ */
+
+
+/**
+ * Deletes duplicate images
+ */
+function deleteDuplicateImages(targetDirectory){
+    // Storing the hashes for each image in a set
+    const fileHashes = new Set();
+    const files = fs.readdirSync(targetDirectory)
+
+    // TODO: Understand the code as you write it, don't just copy it. 
+
+}
 
 console.log("kek")
