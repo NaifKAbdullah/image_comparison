8e2b3d270a7320b37d2e39603bedb2c5626b3bc8
NaifKAbdullah <Naif-Abdullah@outlook.sa>
2024-01-06 00:29:51 -0600

    added size and dir length comparison

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ const targetDirectory = "./Put_Images_Here"
 /**
  * Funciton to calculate the hash for an image
  */
+function calculateHash(picturePath){
+    // TODO: What does this do? Ask GPT
+    const fileBuffer = fs.readFileSync(picturePath)
+    const hashSum = crypto.createHash('sha256')
+    hashSum.update(fileBuffer)
+    return hashSum.digest('hex')
+}
 
 
 /**
@@ -18,11 +25,37 @@ const targetDirectory = "./Put_Images_Here"
  */
 function deleteDuplicateImages(targetDirectory){
     // Storing the hashes for each image in a set
-    const fileHashes = new Set();
-    const files = fs.readdirSync(targetDirectory)
+    const picturesAndTheirInfo = {}
+    // For reading a directory and its contents
+    // TODO: Console.log 'files' to see what it returns, seems to be an array of file names in the directory
+    const picturesPaths = fs.readdirSync(targetDirectory)
 
-    // TODO: Understand the code as you write it, don't just copy it. 
+    picturesPaths.forEach(picturePath => {
+        // Constructing the full directory
+        const fullPathToPicture = path.join(targetDirectory, picturePath)
+        // Generating the hash for that picture
+        const pictureHash = calculateHash(fullPathToPicture)
+        const pictureSize = fs.statSync(fullPathToPicture).size
 
+        if (picturesAndTheirInfo[pictureHash]){ // A duplicate?
+            console.log("Trig")
+            const existingPicture = picturesAndTheirInfo[pictureHash]
+            if (pictureSize < existingPicture.size || picturePath.length < existingPicture.name.length){
+                // Delete existing picture
+                fs.unlinkSync(existingPicture.path);
+                console.log(`Deleted: ${existingPicture.path}`);
+                // Update the stored picture details
+                pictureDetails[pictureHash] = { path: fullPathToPicture, size: pictureSize, name: picturePath };
+            } else {
+                // Delete new picture
+                fs.unlinkSync(fullPathToPicture);
+                console.log(`Deleted: ${fullPathToPicture}`);
+            }
+        }else[ // Not a duplicate
+            // Not a duplicate, add it to the details object
+            pictureDetails[pictureHash] = { path: fullPathToPicture, size: pictureSize, name: picturePath }
+        ]
+    })
 }
 
-console.log("kek")
+deleteDuplicateImages(targetDirectory)
